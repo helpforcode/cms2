a292256ddd2a787d845fcc073ca8fd61fed73766
Doge <doge@home.com>
2022-02-13 12:15:19 +0800

    add axios

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,20 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import Vue from 'vue'
+export default {
+  name: 'About',
+  methods: {
+
+  },
+  mounted() {
+    Vue.axios.get('http://httpbin.org/get').then(response => {
+      console.log(response)
+    }).catch(err => {
+      console.log('err', err)
+    })
+  }
+}
+</script>
