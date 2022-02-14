2f4b2f35762df6513016998dad78fff87ac01864
lex <chaoquan_l@gzleihou.cn>
2022-02-14 18:07:32 +0800

    reference

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,7 @@ module.exports = {
       scss: {
         // @/ 是 src/ 的别名
         // 注意：在 sass-loader v7 中，这个选项名是 "data"
+        // https://www.jianshu.com/p/6ea3339c6579
         additionalData: ` 
                 @import "@nutui/nutui/dist/styles/index.scss";
                 `,
