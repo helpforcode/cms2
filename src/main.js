189ed79f688c4ec6e80c1328ef8ae0aa25c2da78
lex <chaoquan_l@gzleihou.cn>
2022-02-16 11:01:12 +0800

    vant

--- src/main.js
+++ src/main.js
@@ -9,7 +9,10 @@ import vuetify from './plugins/vuetify'
 import NutUI from '@nutui/nutui'
 import '@nutui/nutui/dist/nutui.css'
 import '@/permission'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+Vue.use(Vant)
 NutUI.install(Vue)
 
 Vue.config.productionTip = false
--- src/views/article/Article.vue
+++ src/views/article/Article.vue
@@ -1,57 +1,37 @@
 <template>
   <div class="articles d-flex flex-column">
-    <nut-infiniteloading
-        @loadmore="nextPage()"
-        :is-show-mod="true"
-        :has-more="pageParams.hasMore"
-        :is-loading="loading"
-        :threshold="200"
-        :use-window="false"
+    <van-list
+        v-model="loading"
+        :finished="finished"
+        @load="nextPage()"
+        :finished-text="'No more data'"
     >
-      <div class="article d-flex flex-row align-center"
-           v-for="article in articles" :key="article.id"
-      >
-        <!--      <v-menu >-->
-        <!--        <template class="ico" v-slot:activator="{on, attrs}">-->
-        <!--          <fa-icon icon="fa-solid fa-ellipsis-vertical"-->
-        <!--                   v-bind="attrs"-->
-        <!--                   v-on="on"-->
-        <!--          />-->
-        <!--        </template>-->
-
-        <!--        <v-list>-->
-        <!--          <v-list-item v-for="(item, index) in ops" :key="index">-->
-        <!--            <v-list-item-title>-->
-        <!--              {{item.op}}-->
-        <!--            </v-list-item-title>-->
-        <!--          </v-list-item>-->
-        <!--        </v-list>-->
-        <!--      </v-menu>-->
-        <div class="ico d-flex">
-          <v-speed-dial
-              bottom
-              left
-              direction="right"
-          >
-            <template v-slot:activator>
-              <v-icon>fa-ellipsis-vertical</v-icon>
-            </template>
-            <v-btn
-                fab
-                dark
-                small
-                v-for="(item, index) in ops" :key="index"
-            >
-              <v-icon class="ops" >{{item.icon}}</v-icon>
-            </v-btn>
-          </v-speed-dial>
-        </div>
-
-        <v-spacer/>
-
-        <div>{{article.title}}</div>
-      </div>
-    </nut-infiniteloading>
+      <van-cell
+          v-for="article in articles" :key="article.id" :value="article.title">
+          <template #title>
+<!--            <van-icon class-prefix="fa" name="ellipsis"></van-icon>-->
+            <div class="ico d-flex">
+              <v-speed-dial
+                  bottom
+                  left
+                  direction="right"
+              >
+                <template v-slot:activator>
+                  <v-icon>fa-ellipsis</v-icon>
+                </template>
+                <v-btn
+                    fab
+                    dark
+                    small
+                    v-for="(item, index) in ops" :key="index"
+                >
+                  <v-icon class="ops" >{{item.icon}}</v-icon>
+                </v-btn>
+              </v-speed-dial>
+            </div>
+          </template>
+      </van-cell>
+    </van-list>
 
     <div class="bottom-bar">
       <v-fab-transition>
@@ -78,8 +58,9 @@ export default {
   name: 'Article',
   data() {
     return {
+      finished: false,
       loading: false,
-      pageParams: {page: 0, size: 15, hasMore: false},
+      pageParams: {page: 0, size: 10},
       articles: [],
       ops: [{op: 'edit', icon: 'fa-pen'}, {op: 'delete', icon: 'fa-trash-can'}],
       timer: null,
@@ -87,7 +68,6 @@ export default {
   },
   methods: {
     nextPage(curPage) {
-      this.loading = true
       this.timer = setTimeout(() => {
         curPage = curPage || this.pageParams.page
         this.pageParams.page = curPage + 1;
@@ -95,16 +75,14 @@ export default {
         article.articles(this.pageParams).then(response => {
           let data = response.data.data
           let pageInfo = data.pageInfo
-          this.articles = data.content
-          this.pageParams.hasMore = curPage < pageInfo.totalPages
+          this.articles = this.articles.concat(data.content)
+          this.loading = false
+          this.finished = curPage >= pageInfo.totalPages
         })
-        this.loading = false
-      }, 100)
+      }, 1000)
     }
   },
   mounted() {
-    let curPage = Number(this.$route.query.page || this.pageParams.page) - 1
-    this.nextPage(curPage)
   },
   destroyed() {
     clearTimeout(this.timer)
